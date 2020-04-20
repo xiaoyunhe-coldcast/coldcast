359c58c5d0e80e3a8d2d6943224c8a733652f95e
xiao <854609899@qq.com>
2020-04-20 17:11:27 +0800

    添加数据库

---
+++ src/resource/user.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL COMMENT '主键ID',
+  `name` varchar(30) DEFAULT NULL COMMENT '姓名',
+  `age` int(11) DEFAULT NULL COMMENT '年龄',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+####
+
+LUES (1, 'Jone', 18, 'test1@baomidou.com');
+INSERT INTO `user` VALUES (2, 'Jack', 20, 'test2@baomidou.com');
+INSERT INTO `user` VALUES (3, 'Tom', 28, 'test3@baomidou.com');
+INSERT INTO `user` VALUES (4, 'Sandy', 21, 'test4@baomidou.com');
+INSERT INTO `user` VALUES (5, 'Billie', 24, 'test5@baomidou.com');
+INSERT INTO `user` VALUES (6, 'jone', 66, 'test1@baomidou.com');
